1c128e731f8e3b90f49f52a1c2e41cb0646f98b4
haocetu <nhathao230502@gmail.com>
2024-03-19 14:05:19 +0700

    Testing git add trong thu muc con

--- PRN221_GroupProjectBookParty/DAO/FeedBackDAO.cs
+++ PRN221_GroupProjectBookParty/DAO/FeedBackDAO.cs
@@ -13,7 +13,7 @@ namespace DAO
         private readonly BookingPartyContext dbContext = null;
         public FeedBackDAO()
         {
-            if(dbContext == null)
+            if (dbContext == null)
             {
                 dbContext = new BookingPartyContext();
             }
@@ -28,6 +28,42 @@ namespace DAO
                 }
                 return instance;
             }
+        }
+        public void CreateFeedBack(FeedBack feedback)
+        {
+            FeedBack fb = GetFeedBackById(feedback.Id);
+            if (fb == null)
+            {
+                dbContext.Add(feedback);
+                dbContext.SaveChanges();
+            }
+        }
+        public FeedBack GetFeedBackById(int id)
+        {
+            return dbContext.FeedBacks.FirstOrDefault(x => x.Id == id);
+        }
+        public void UpdateFeedBack(FeedBack feedback)
+        {
+            FeedBack fb = GetFeedBackById(feedback.Id);
+            if (fb != null)
+            {
+                dbContext.Update(feedback);
+                dbContext.SaveChanges();
+            }
+        }
+        public void DeleteFeedBack(FeedBack feedback)
+        {
+            FeedBack fb = GetFeedBackById(feedback.Id);
+            if (fb != null)
+            {
+                dbContext.Remove(feedback);
+                dbContext.SaveChanges();
+            }
+
+        }
+        public List<FeedBack> GetListFeedBack()
+        {
+            return dbContext.FeedBacks.ToList();
         }
     }
 }
