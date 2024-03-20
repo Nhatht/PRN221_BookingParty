92cde66a32b0ca7af09271896b2890fcea61688b
Phúcc <lamminhphuc285@gmail.com>
2024-03-21 00:05:59 +0700

    add BlogPost DAO,Repo, Service

--- PRN221_GroupProjectBookParty/DAO/BlogPostDAO.cs
+++ PRN221_GroupProjectBookParty/DAO/BlogPostDAO.cs
@@ -1,7 +1,9 @@
 ﻿using BO;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -29,5 +31,84 @@ namespace DAO
                 return instance;
             }
         }
+        public async Task<List<BlogPost>> GetAll() => await dbContext.BlogPosts.AsNoTracking().Include(x => x.Account).AsNoTracking().ToListAsync();
+        public async Task<bool> AddBlogPost(BlogPost BlogPost)
+        {
+            try
+            {
+                await dbContext.BlogPosts.AddAsync(BlogPost);
+                await dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public async Task<bool> DeleteBlogPost(int id)
+        {
+            BlogPost BlogPost = GetBlogPostById(id);
+            try
+            {
+                dbContext.BlogPosts.Remove(BlogPost);
+                await dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public BlogPost GetBlogPostById(int id)
+        {
+            return dbContext.BlogPosts.AsNoTracking().FirstOrDefault(x => x.Id == id);
+        }
+
+        public async Task<bool> EditBlogPost(BlogPost BlogPost)
+        {
+            var blog = await dbContext.BlogPosts.AsNoTracking().FirstOrDefaultAsync(x => x.Id == BlogPost.Id);
+            if (blog != null)
+            {
+                blog.Heading = BlogPost.Heading;
+                blog.PageTitle = BlogPost.PageTitle;
+                blog.Content = BlogPost.Content;
+                blog.ShortDescription = BlogPost.ShortDescription;
+                blog.ImageUrl = BlogPost.ImageUrl;
+                blog.PublishedDate = BlogPost.PublishedDate;
+                blog.Visible = BlogPost.Visible;
+                blog.AccountId = BlogPost.AccountId;
+            }
+            try
+            {
+                dbContext.BlogPosts.Update(blog);
+                await dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<List<BlogPost>> GetBlogPostsListByFilter(Expression<Func<BlogPost, bool>>? filter = null)
+        {
+            IQueryable<BlogPost> query = dbContext.BlogPosts;
+            if (filter != null)
+            {
+                query = query.Include(x => x.Account).Where(filter);
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task<BlogPost> GetBlogPostsByFilter(Expression<Func<BlogPost, bool>>? filter = null)
+        {
+            IQueryable<BlogPost> query = dbContext.BlogPosts;
+            if (filter != null)
+            {
+                query = query.AsNoTracking().Include(x => x.Account).AsNoTracking().Where(filter);
+            }
+            return await query.FirstOrDefaultAsync();
+        }
     }
 }
+
--- PRN221_GroupProjectBookParty/PartyService/BlogPostService.cs
+++ PRN221_GroupProjectBookParty/PartyService/BlogPostService.cs
@@ -1,7 +1,9 @@
-﻿using PartyRepository;
+﻿using BO;
+using PartyRepository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,5 +16,19 @@ namespace PartyService
         {
             _blogPostRepo = new BlogPostRepo();
         }
+        private readonly IBlogPostRepo _repo = null;
+
+        public async Task<bool> AddBlogPost(BlogPost BlogPost) => await _repo.AddBlogPost(BlogPost);
+
+        public async Task<bool> DeleteBlogPost(int id) => await _repo.DeleteBlogPost(id);
+
+        public async Task<bool> EditBlogPost(BlogPost BlogPost) => await _repo.EditBlogPost(BlogPost);
+
+        public async Task<List<BlogPost>> GetAll() => await _repo.GetAll();
+
+        public async Task<List<BlogPost>> GetBlogPostsListByFilter(Expression<Func<BlogPost, bool>>? filter = null)
+            => await _repo.GetBlogPostsListByFilter(filter);
+        public async Task<BlogPost> GetBlogPostsByFilter(Expression<Func<BlogPost, bool>>? filter = null)
+            => await _repo.GetBlogPostsByFilter(filter);
     }
 }
