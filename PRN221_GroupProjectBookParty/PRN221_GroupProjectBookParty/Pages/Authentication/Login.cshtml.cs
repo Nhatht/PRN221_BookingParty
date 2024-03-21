8d5e022eb08fc583aa58c2d9e9a3e5ceefd43552
Nguyễn Anh Duy <duynguyenbt21093@gmail.com>
2024-03-21 22:01:03 +0700

    update form login, register

--- PRN221_GroupProjectBookParty/PRN221_GroupProjectBookParty/Pages/Authentication/Login.cshtml.cs
+++ PRN221_GroupProjectBookParty/PRN221_GroupProjectBookParty/Pages/Authentication/Login.cshtml.cs
@@ -23,26 +23,19 @@ namespace PRN221_GroupProjectBookParty.Pages.Authentication
 
         public async Task<IActionResult> OnPostAsync()
         {
-            if (ModelState.IsValid)
-            {
                 // Kiểm tra đăng nhập sử dụng dữ liệu từ Account model
                 if (_accountService.Login(Account.Email, Account.Password))
                 {
                     TempData["Email"] = Account.Email;
                     var account = _accountService.GetAccountByEmail(Account.Email);
-                    if (account.Role.Equals("Admin")) { return RedirectToPage("/Admin/Index"); }
+                    if (account.Role.Equals("Admin")) { return RedirectToPage("/Admin/AdminAccount"); }
                     else if (account.Role.Equals("Host")) { return RedirectToPage("/Teacher_page/Index"); }
                     else if (account.Role.Equals("Guest")) { return RedirectToPage("/Student_page/Index"); }
-                    
-                }
-                else
-                {
-                    // Đăng nhập thất bại, thêm lỗi vào ModelState
-                    ModelState.AddModelError(string.Empty, "Invalid email or password");
-                }
-            }
 
-            // ModelState không hợp lệ, trả về trang hiện tại
+            }
+            
+
+            
             return Page();
         }
     }
