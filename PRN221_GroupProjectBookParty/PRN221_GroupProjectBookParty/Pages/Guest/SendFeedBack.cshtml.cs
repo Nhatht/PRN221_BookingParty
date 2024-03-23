c867f4313809c6683ae4c66c029f8f71d0d91b7d
haocetu <nhathao230502@gmail.com>
2024-03-23 20:58:54 +0700

    Send FB

---
+++ PRN221_GroupProjectBookParty/PRN221_GroupProjectBookParty/Pages/Guest/SendFeedBack.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN221_GroupProjectBookParty.Pages.Guest
+{
+    public class SendFeedBackModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
