667c64edbc4a76025c02a5a714d01cbf2f446e97
Nguyễn Anh Duy <duynguyenbt21093@gmail.com>
2024-03-24 13:35:25 +0700

    Password hash, Fix register UI

--- PRN221_GroupProjectBookParty/DAO/obj/Debug/net6.0/DAO.AssemblyInfo.cs
+++ PRN221_GroupProjectBookParty/DAO/obj/Debug/net6.0/DAO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+394507cdf25f1d5b7184c490aaf2f33cb08e1835")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad2ac568d59bd6aec5ea4d8e5f706c510bf7df08")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ PRN221_GroupProjectBookParty/PRN221_GroupProjectBookParty/Pages/Authentication/PasswordHashing.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PRN221_GroupProjectBookParty.Pages.Authentication
+{
+    public class PasswordHashing
+    {
+        // Hàm để hash mật khẩu sử dụng thuật toán SHA256
+        public static string HashPassword(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // Chuyển đổi mật khẩu từ kiểu string sang mảng byte
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                // Convert byte array thành một chuỗi hex string
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        // Hàm để kiểm tra mật khẩu đã hash có khớp với mật khẩu gốc không
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            // Hash mật khẩu và so sánh với mật khẩu đã hash
+            string hashOfInput = HashPassword(password);
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+            return comparer.Compare(hashOfInput, hashedPassword) == 0;
+        }
+    }
+}
