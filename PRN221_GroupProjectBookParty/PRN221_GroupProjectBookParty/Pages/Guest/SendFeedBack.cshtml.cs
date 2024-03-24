5b994d1742f170bfd6c147237f9479b198556b5a
haocetu <nhathao230502@gmail.com>
2024-03-25 04:19:48 +0700

    Done 90% FB

--- PRN221_GroupProjectBookParty/PRN221_GroupProjectBookParty/Pages/Guest/SendFeedBack.cshtml.cs
+++ PRN221_GroupProjectBookParty/PRN221_GroupProjectBookParty/Pages/Guest/SendFeedBack.cshtml.cs
@@ -9,54 +9,54 @@ using PartyService.ViewModel;
 
 namespace PRN221_GroupProjectBookParty.Pages.Guest
 {
-	public class SendFeedBackModel : PageModel
-	{
-		private readonly IFeedBackService _feedbackService;
-		private readonly IBookingService _bookingService;
-		private readonly IPartysService _partysService;
-		private readonly IAccountService _accountService;
-		public SendFeedBackModel(IFeedBackService feedbackService, IBookingService bookingService, IPartysService partysService, IAccountService accountService)
-		{
-			_feedbackService = feedbackService;
-			_bookingService = bookingService;
-			_partysService = partysService;
-			_accountService = accountService;
-		}
-		public IActionResult OnGetAsync(int id)
-		{
-			id = 1;
-			var booking = _bookingService.GetBookingById(id);
-			if (booking != null)
-			{
-				ViewData["GuestId"] = booking.Result.GuestId;
-				ViewData["PartyId"] = booking.Result.PartyId;
-			}
-			var party = _partysService.GetPartyById(booking.Result.PartyId);
-			if (party != null)
-			{
-				ViewData["PartyName"] = party.Result.Name;
-			}
-			var acc = _accountService.GetAccountById(booking.Result.GuestId);
-			if (acc != null)
-			{
-				ViewData["GuestName"] = acc.UserName;
-			}
-			return Page();
-		}
-		[BindProperty]
-		public FeedBack FeedBack { get; set; }
-		public async Task<IActionResult> OnPostAsync(int id)
-		{
-			var feedback = new FeedBack
-			{
-				Id = FeedBack.Id,
-				GuestId = FeedBack.GuestId,
-				PartyId = FeedBack.PartyId,
-				Comment = FeedBack.Comment,
-				ReviewDate = FeedBack.ReviewDate,
-			};
-			_feedbackService.CreateFeedBack(feedback);
-			return RedirectToPage("./Index");
-		}
-	}
+    public class SendFeedBackModel : PageModel
+    {
+        private readonly IFeedBackService _feedbackService;
+        private readonly IBookingService _bookingService;
+        private readonly IPartysService _partysService;
+        private readonly IAccountService _accountService;
+        public SendFeedBackModel(IFeedBackService feedbackService, IBookingService bookingService, IPartysService partysService, IAccountService accountService)
+        {
+            _feedbackService = feedbackService;
+            _bookingService = bookingService;
+            _partysService = partysService;
+            _accountService = accountService;
+        }
+        public IActionResult OnGetAsync(int id)
+        {
+            //id = 1;
+            var booking = _bookingService.GetBookingById(id);
+            if (booking != null)
+            {
+                ViewData["GuestId"] = booking.Result.GuestId;
+                ViewData["PartyId"] = booking.Result.PartyId;
+            }
+            var party = _partysService.GetPartyById(booking.Result.PartyId);
+            if (party != null)
+            {
+                ViewData["PartyName"] = party.Result.Name;
+            }
+            var acc = _accountService.GetAccountById(booking.Result.GuestId);
+            if (acc != null)
+            {
+                ViewData["GuestName"] = acc.UserName;
+            }
+            return Page();
+        }
+        [BindProperty]
+        public FeedBack FeedBack { get; set; }
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            var feedback = new FeedBack
+            {
+                Id = FeedBack.Id,
+                GuestId = FeedBack.GuestId,
+                PartyId = FeedBack.PartyId,
+                Comment = FeedBack.Comment,
+                ReviewDate = FeedBack.ReviewDate,
+            };
+            _feedbackService.CreateFeedBack(feedback);
+            return RedirectToPage("/Guest/PartyDetail", new { id = FeedBack.PartyId });
+        }
+    }
 }
--- PRN221_GroupProjectBookParty/PartyService/obj/Debug/net6.0/PartyService.AssemblyInfo.cs
+++ PRN221_GroupProjectBookParty/PartyService/obj/Debug/net6.0/PartyService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PartyService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ed75b82243caffe6ab71834dfa859084e61b14c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0639ff08096f7a520c2149806a0546dff35dd6c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PartyService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PartyService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
